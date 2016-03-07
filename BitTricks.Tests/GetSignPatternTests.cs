6239de8f5c585ac8c0a175048be8a005293551e8
Licshee <licshee@yen.ai>
2016-03-07 23:53:07 +0800

    more tests & formatting tests

--- BitTricks.Tests/GetSignPatternTests.cs
+++ BitTricks.Tests/GetSignPatternTests.cs
@@ -10,7 +10,7 @@ public class GetSignPatternTests
     {
         sbyte b;
         for (b = 0; b < sbyte.MaxValue; b++)
-            Assert.Equal((sbyte)0, b.GetSignPattern());
+            Assert.Equal((sbyte)+0, b.GetSignPattern());
         Assert.Equal(0, sbyte.MaxValue.GetSignPattern());
         b = 0;
         while (b-- > sbyte.MinValue)
@@ -22,7 +22,7 @@ public class GetSignPatternTests
     {
         short s;
         for (s = 0; s < short.MaxValue; s++)
-            Assert.Equal((short)0, s.GetSignPattern());
+            Assert.Equal((short)+0, s.GetSignPattern());
         Assert.Equal(0, short.MaxValue.GetSignPattern());
         s = 0;
         while (s-- > short.MinValue)
@@ -33,9 +33,9 @@ public class GetSignPatternTests
     [Fact]
     public void ForInt16Alive()
     {
-        Assert.Equal(0, short.MaxValue.GetSignPattern());
-        Assert.Equal(0, ((short)1).GetSignPattern());
-        Assert.Equal(0, ((short)0).GetSignPattern());
+        Assert.Equal(+0, short.MaxValue.GetSignPattern());
+        Assert.Equal(+0, ((short)+1).GetSignPattern());
+        Assert.Equal(+0, ((short)+0).GetSignPattern());
         Assert.Equal(-1, ((short)-1).GetSignPattern());
         Assert.Equal(-1, short.MinValue.GetSignPattern());
 
@@ -43,7 +43,7 @@ public class GetSignPatternTests
         for (var i = 0; i < RandomTestCount; i++)
         {
             var v = (short)(short.MaxValue * rnd.NextDouble()) + 1;
-            Assert.Equal(0, v.GetSignPattern());
+            Assert.Equal(+0, (+v).GetSignPattern());
             Assert.Equal(-1, (-v).GetSignPattern());
         }
     }
@@ -51,9 +51,9 @@ public class GetSignPatternTests
     [Fact]
     public void ForInt32()
     {
-        Assert.Equal(0, int.MaxValue.GetSignPattern());
-        Assert.Equal(0, 1.GetSignPattern());
-        Assert.Equal(0, 0.GetSignPattern());
+        Assert.Equal(+0, int.MaxValue.GetSignPattern());
+        Assert.Equal(+0, (+1).GetSignPattern());
+        Assert.Equal(+0, (+0).GetSignPattern());
         Assert.Equal(-1, (-1).GetSignPattern());
         Assert.Equal(-1, int.MinValue.GetSignPattern());
 
@@ -61,7 +61,7 @@ public class GetSignPatternTests
         for (var i = 0; i < RandomTestCount; i++)
         {
             var v = rnd.Next() + 1;
-            Assert.Equal(0, v.GetSignPattern());
+            Assert.Equal(+0, (+v).GetSignPattern());
             Assert.Equal(-1, (-v).GetSignPattern());
         }
     }
@@ -69,9 +69,9 @@ public class GetSignPatternTests
     [Fact]
     public void ForInt64()
     {
-        Assert.Equal(0, long.MaxValue.GetSignPattern());
-        Assert.Equal(0, ((long)1).GetSignPattern());
-        Assert.Equal(0, ((long)0).GetSignPattern());
+        Assert.Equal(+0, long.MaxValue.GetSignPattern());
+        Assert.Equal(+0, ((long)+1).GetSignPattern());
+        Assert.Equal(+0, ((long)+0).GetSignPattern());
         Assert.Equal(-1, ((long)-1).GetSignPattern());
         Assert.Equal(-1, long.MinValue.GetSignPattern());
 
@@ -79,7 +79,7 @@ public class GetSignPatternTests
         for (var i = 0; i < RandomTestCount; i++)
         {
             var v = (long)(long.MaxValue * rnd.NextDouble()) + 1;
-            Assert.Equal(0, v.GetSignPattern());
+            Assert.Equal(+0, (+v).GetSignPattern());
             Assert.Equal(-1, (-v).GetSignPattern());
         }
     }
--- BitTricks.Tests/ZigZagTests.cs
+++ BitTricks.Tests/ZigZagTests.cs
@@ -15,6 +15,22 @@ public class ZigZagTests
     [Fact]
     public void ForInt32()
     {
+        Assert.Equal(0u, (+0).Zig());
+        Assert.Equal(1u, (-1).Zig());
+        Assert.Equal(2u, (+1).Zig());
+        Assert.Equal(3u, (-2).Zig());
+
+        Assert.Equal(uint.MaxValue - 1, int.MaxValue.Zig());
+        Assert.Equal(uint.MaxValue - 0, int.MinValue.Zig());
+
+        Assert.Equal(+0, 0u.Zag());
+        Assert.Equal(-1, 1u.Zag());
+        Assert.Equal(+1, 2u.Zag());
+        Assert.Equal(-2, 3u.Zag());
+
+        Assert.Equal(int.MaxValue, (uint.MaxValue - 1).Zag());
+        Assert.Equal(int.MinValue, (uint.MaxValue - 0).Zag());
+
         var rnd = new Random();
         for (var i = 0; i < RandomTestCount; i++)
         {
@@ -35,6 +51,22 @@ public class ZigZagTests
     [Fact]
     public void ForInt64()
     {
+        Assert.Equal(0uL, (+0L).Zig());
+        Assert.Equal(1uL, (-1L).Zig());
+        Assert.Equal(2uL, (+1L).Zig());
+        Assert.Equal(3uL, (-2L).Zig());
+
+        Assert.Equal(ulong.MaxValue - 1, long.MaxValue.Zig());
+        Assert.Equal(ulong.MaxValue - 0, long.MinValue.Zig());
+
+        Assert.Equal(+0L, 0uL.Zag());
+        Assert.Equal(-1L, 1uL.Zag());
+        Assert.Equal(+1L, 2uL.Zag());
+        Assert.Equal(-2L, 3uL.Zag());
+
+        Assert.Equal(long.MaxValue, (ulong.MaxValue - 1).Zag());
+        Assert.Equal(long.MinValue, (ulong.MaxValue - 0).Zag());
+
         var rnd = new Random();
         for (var i = 0; i < RandomTestCount; i++)
         {
